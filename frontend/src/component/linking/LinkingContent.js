11cf90c1e01853aa28a7afe9cb387262b3c7890c
Woochang Lee <changi112242@outlook.com>
2023-06-03 23:00:25 +0900

    Update to load linking item list

--- frontend/src/component/linking/LinkingContent.js
+++ frontend/src/component/linking/LinkingContent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { format } from 'date-fns';
 import { DayPicker } from 'react-day-picker';
@@ -10,18 +10,37 @@ import "./LinkingContent.css"
 
 function LinkingContent() {
     const [selectedDay, setSelectedDay] = useState(Date.now());
+    const [todaySurveys, setTodaySurveys] = useState([]);
 
-    const todaySurveys = [
-        { name: "종강총회1", date: '2023.03.24', answer: 10 },
-        { name: "종강총회2", date: '2023.03.24', answer: 1 },
-        { name: "종강총회3", date: '2023.03.24', answer: 12 },
-        { name: "종강총회4", date: '2023.03.24', answer: 34 },
-    ];
+    useEffect(() => {
+        loadTodaySurveys(selectedDay);
+    }, []);
+
+    const loadTodaySurveys = async (date) => {
+        const response = await fetch(`/data/LinkingItemList.json`, {
+                headers : { 
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        );
+        console.log(response.body)
+        const data = await response.json();
+        if (!data) return;
+
+        if (data[date]) {
+            setTodaySurveys(data[date]);
+        } else {
+            setTodaySurveys([]);
+        }
+    }
 
     function selectDay(day)
     {
-        if (day)
+        if (day) {
+            loadTodaySurveys(format(day, 'yyyy-MM-dd'));
             setSelectedDay(day);
+        }
     }
 
     return (
@@ -29,17 +48,24 @@ function LinkingContent() {
             <div className='list'>
                 <div className='sticky'>
                     <DayPicker mode="single" selected={selectedDay} onSelect={selectDay} />
-                    <p className='selectedDay'>{format(selectedDay, 'yyyy년 MM월 dd일')} 진행 중 설문</p>
+                    <p className='selectedDay'>{format(selectedDay, 'yyyy년 MM월 dd일')} 예정된 설문</p>
                     <div className='linking-list'>
                         {
                             todaySurveys && todaySurveys.map((survey) => (
                                 <LinkingListItem
-                                    key = {survey.name}
+                                    key = {survey.id}
                                     name={survey.name}
-                                    date={survey.date}
                                     answer={survey.answer}
                                 />
                             ))
+                        }
+                        {
+                            todaySurveys.length === 0 && (
+                                <div className='empty'>
+                                    <img src={process.env.PUBLIC_URL + '/images/empty.png'} alt="empty list"/>
+                                    <p>예정된 설문이 없습니다.</p>
+                                </div>
+                            )
                         }
                     </div>
                 </div>
--- frontend/src/component/linking/PlaceWindow.js
+++ frontend/src/component/linking/PlaceWindow.js
@@ -13,13 +13,12 @@ export default function PlaceWindow(props){
     const [image, setImage] = useState([]);
     
     const getData = async (name) => {
-        console.log(name);
         const response = await fetch(`/data/place/${encodeURIComponent(name)}.json`, {
-          headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           }
-        }
+                headers : { 
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
         );
         return response.json();
       }
