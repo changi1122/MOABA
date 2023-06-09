0a389c63869498d2be410e1331be8d721bee6296
sori9899 <hyun989969@gmail.com>
2023-06-09 22:56:54 +0900

    Add MOABA info window

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -4,19 +4,30 @@ import Sidebar from '../component/Sidebar';
 import IntroShow from '../component/main/IntroShow';
 import AccessShow from '../component/main/AccessShow';
 
+import "./MainPageStyle.css";
+
 function MainPage(){
 
     return (
         <div className="horazion">
+            <div className="MainPage-Style">
+                <Sidebar 
+                    width={"100%"}
+                    height={"100%"}
+                    position="center" 
+                    content={IntroShow()} 
+                    isShow={false}
+                />
+
+
+                <div className="Testset">
+                    JHSDHF
+                </div>
+            </div>
             <Sidebar 
-                width={"100%"}
-                position="center" 
-                content={IntroShow()} 
-                isShow={false}
-            />
-            <Sidebar 
-                width={"400px"}
+                width={"300px"}
                 color="white-blue"
+                fix="fixed"
                 position="center"
                 content={AccessShow()}
                 isShow={false}
