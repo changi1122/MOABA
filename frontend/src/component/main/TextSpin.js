dc4ecedc663d61596d603f26c7d730af75764112
Woochang Lee <changi112242@outlook.com>
2023-06-10 01:45:43 +0900

    Update intro page design

--- frontend/src/component/main/TextSpin.js
+++ frontend/src/component/main/TextSpin.js
@@ -6,8 +6,6 @@ function TextSpin( {text1, text2, text3}){
     
     return(
         <div className="Spinner">
-            <p>&#123;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-
             <div className="Spinner-body">
                 <h1 className="Spinner-data h1">
                     <span>{text1}</span>
@@ -21,8 +19,6 @@ function TextSpin( {text1, text2, text3}){
                     <span>{text3}</span>
                 </h1>
             </div>
-
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&#125;</p>
         </div>
     );
 }
